ece4d0036738637bfb46ae922ec6d4b877a5f55e
Rhett <rhett.hanscom@colorado.edu>
2022-04-14 13:55:42 -0600

    shuffle data a bit

---
+++ tests/data_acquisition/config.py
@@ -0,0 +1,9 @@
+access_token="XXXXXXXXXXXXX"
+access_token_secret="XXXXXXXXXXXXX"
+consumer_key="XXXXXXXXXXXXX"
+consumer_secret="XXXXXXXXXXXXX"
+
+default_filename = 'tweet_search.csv'
+default_locations = ['colorado', 'california']
+default_words = ['fire', 'earthquake']
+pickle_file = 'mock_api.pickle'
